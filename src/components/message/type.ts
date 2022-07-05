4c5467f2768f785a12e7ead33de1b3544adff091
13106728392 <13106728392@163.com>
2022-07-05 16:21:27 +0800

    message添加样式修改

---
+++ src/components/message/type.ts
@@ -0,0 +1,15 @@
+export const icon = {
+  info: "c-icon-info info",
+  error: "c-icon-c-circle error",
+  success: "c-icon-check-circle success",
+  warning: "c-icon-alert-triangle warning",
+  loading: "c-icon-loader loading",
+};
+
+export const className = {
+  info: "message-info",
+  error: "message-error",
+  success: "message-success",
+  warning: "message-warning",
+  loading: "message-loading",
+};
