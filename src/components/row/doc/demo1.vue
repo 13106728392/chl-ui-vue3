24ecb9d8fbbd39272124879731321ad38ca26156
13106728392 <13106728392@163.com>
2022-11-15 16:52:16 +0800

    栅栏组件md修改

---
+++ src/components/row/doc/demo1.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div class="components-grid-demo">
+    <c-row>
+      <c-col :span="24" class="color1">24: 100%</c-col>
+    </c-row>
+    <c-row>
+      <c-col :span="12" class="color2">12: 50%</c-col>
+      <c-col :span="12" class="color3">12: 50%</c-col>
+    </c-row>
+    <c-row>
+      <c-col :span="8" class="color1">8: 33.33%</c-col>
+      <c-col :span="8" class="color2">8: 33.33%</c-col>
+      <c-col :span="8" class="color1">8: 33.33%</c-col>
+    </c-row>
+    <c-row>
+      <c-col :span="6" class="color3">6: 25%</c-col>
+      <c-col :span="6" class="color2">6: 25%</c-col>
+      <c-col :span="6" class="color3">6: 25%</c-col>
+      <c-col :span="6" class="color2">6: 25%</c-col>
+    </c-row>
+    <c-row>
+      <c-col :span="16" class="color1">16: 66.66%</c-col>
+      <c-col :span="8" class="color2">8: 33.33%</c-col>
+    </c-row>
+  </div>
+</template>
---
+++ src/components/row/doc/demo6.vue
@@ -0,0 +1,13 @@
+
+<template>
+     <div class="components-grid-demo">
+    <c-row type="flex">
+      <c-col :span="6" :order="4" class="color1"><div>1 | order-4</div></c-col>
+      <c-col :span="6" :order="3" class="color2"><div>2 | order-3</div></c-col>
+      <c-col :span="6" :order="2" class="color3"><div>3 | order-2</div></c-col>
+      <c-col :span="6" :order="1" class="color4"><div>4 | order-1</div></c-col>
+    </c-row>
+     </div>
+  </template>
+  
+  
