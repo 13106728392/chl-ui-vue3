85c7bd64df7afedfa1c422f4469c958d3b1eb3a0
13106728392 <13106728392@163.com>
2022-11-17 15:08:19 +0800

    轮播图组件md修改

---
+++ src/components/carousel/doc/demo1.vue
@@ -0,0 +1,9 @@
+
+<template>
+    <c-carousel width="500px" height="250px">
+      <c-carousel-item :class="`demo-carousel color${item}`" v-for="item in 4">
+        {{item}}
+      </c-carousel-item>
+    </c-carousel>
+  </template>
+  
