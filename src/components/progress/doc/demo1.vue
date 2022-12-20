0f4245afeefbf538f1a8c0959b1557ba381d3065
13106728392 <13106728392@163.com>
2022-12-20 15:45:11 +0800

    添加progress组件

---
+++ src/components/progress/doc/demo1.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="demo-progress">
+      <c-progress :percentage="50" />
+      <c-progress :percentage="100" :format="format"></c-progress>
+      <c-progress :percentage="100" status="success"></c-progress>
+      <c-progress :percentage="100" status="warning"></c-progress>
+      <c-progress :percentage="50" status="error"></c-progress>
+    </div>
+  </template>
+  
+  <script setup>
+  const format = (percentage) => (percentage === 100 ? '满' : `${percentage}%`)
+  </script>
+  
+  <style scoped>
+  .demo-progress .c-progress--line {
+    margin-bottom: 15px;
+    width: 350px;
+  }
+  </style>
---
+++ src/components/progress/doc/demo4.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="demo-progress">
+      <c-progress type="circle" :percentage="0" />
+      <c-progress type="circle" :percentage="25" />
+      <c-progress type="circle" :percentage="100" status="success" />
+      <c-progress type="circle" :percentage="70" status="warning" />
+      <c-progress type="circle" :percentage="50" status="error" />
+    </div>
+  </template>
+  
+  <style scoped>
+  .demo-progress .c-progress--circle {
+    margin-right: 15px;
+  }
+  </style>
